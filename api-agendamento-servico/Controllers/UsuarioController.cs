b993b338fa2cf509c0d6d6acd84e956cea0bec4f
RENAN_SILVA\renan <renan.costas@hotmail.com>
2024-04-15 23:32:10 -0300

    Inserção da função de inserir usuário.

--- api-agendamento-servico/Controllers/UsuarioController.cs
+++ api-agendamento-servico/Controllers/UsuarioController.cs
@@ -1,6 +1,7 @@
 ﻿using api_agendamento_servico.Data;
 using api_agendamento_servico.Models;
 using api_agendamento_servico.ViewModels;
+using api_agendamento_servico.ViewModels.Usuarios;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,21 +13,68 @@ namespace api_agendamento_servico.Controllers
         [HttpGet("v1/usuarios")]
         public IActionResult GetUsuarios([FromServices] AgendamentoServicoDataContext context)
         {
-            return Ok(new ResultadoViewModel<List<Usuario>>(context.Usuarios.ToList()));
+            try
+            {
+                return Ok(new ResultadoViewModel<List<Usuario>>(context.Usuarios.ToList()));
+            }
+            catch (Exception ex) 
+            {
+                return StatusCode(500, new ResultadoViewModel<Usuario>("01X01 - Não foi possível obter a lista de usuários!"));
+            }
+            
         }
 
         [HttpGet("v1/usuarios/{id:int}")]
         public async Task<IActionResult> GetUsuarioAsync([FromServices] AgendamentoServicoDataContext context,
             [FromRoute] int id)
         {
-            var usuario = await context
+            try
+            {
+                var usuario = await context
                     .Usuarios
                     .FirstOrDefaultAsync(x => x.Id == id);
 
-            if (usuario == null)
-                return NotFound(new ResultadoViewModel<Usuario>("Conteúdo não encontrado"));
+                if (usuario == null)
+                    return NotFound(new ResultadoViewModel<Usuario>("Conteúdo não encontrado"));
 
-            return Ok(new ResultadoViewModel<Usuario>(usuario));
+                return Ok(new ResultadoViewModel<Usuario>(usuario));
+
+            }
+            catch (Exception ex) 
+            {
+                return StatusCode(500, new ResultadoViewModel<Usuario>("01X02 - Não foi possível obter usuário!"));
+            }
+            
+        }
+
+        [HttpPost("v1/usuarios")]
+        public async Task<IActionResult> PostAsync(
+            [FromBody] EditorUsuariosViewModel model,
+            [FromServices] AgendamentoServicoDataContext context)
+        {
+            
+            try
+            {
+                var usuario = new Usuario
+                {
+                    //Id = 0,
+                    Nome = model.Nome,
+                    Email = model.Email.ToLower(),
+                    Senha = model.Senha
+                };
+                await context.Usuarios.AddAsync(usuario);
+                await context.SaveChangesAsync();
+
+                return Created($"v1/categories/{usuario.Id}", new ResultadoViewModel<Usuario>(usuario));
+            }
+            catch (DbUpdateException ex)
+            {
+                return StatusCode(500, new ResultadoViewModel<Usuario>("01X03 - Não foi possível incluir usuário!"));
+            }
+            catch
+            {
+                return StatusCode(500, new ResultadoViewModel<Usuario>("01X04 - Falha interna no servidor"));
+            }
         }
 
     }
---
+++ api-agendamento-servico/ViewModels/Usuarios/EditorUsuariosViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api_agendamento_servico.ViewModels.Usuarios
+{
+    public class EditorUsuariosViewModel
+    {
+        
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        [StringLength(40, MinimumLength = 3, ErrorMessage = "Este campo deve conter entre 3 e 40 caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O e-mail é obrigatório")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "O e-mail é obrigatório")]
+        public string Senha { get; set; }
+
+    }
+}
